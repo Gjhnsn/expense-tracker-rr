dba15c87af2bc47e91f82a3979a7df4af7a61079
Garrett Johnson <garrettjtrains@gmail.com>
2023-06-07 12:58:23 -0700

    refactor(app): fix mobile layour bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Nav } from './styles'
+import React from "react";
+import { Nav, Wrapper } from "./styles";
 import { BsGithub } from "react-icons/bs";
-
+import Month from "../Month/Month";
 
 const Header = () => {
   return (
-    <Nav>
-      <h1>MoneyMinder</h1>
-       <a href="https://github.com/Gjhnsn" target="_blank">
-        <BsGithub />
+      <Nav>
+        <h1>MoneyMinder</h1>
+        <a href="https://github.com/Gjhnsn" target="_blank">
+          <BsGithub />
         </a>
-    </Nav>
-  )
-}
+      </Nav>
+  );
+};
 
-export default Header
+export default Header;
